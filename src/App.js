180860e6911b3f5560852b4367b341941f2f8474
jgfdevmtn <35707501+jgfdevmtn@users.noreply.github.com>
2018-01-29 00:55:44 -0600

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import './index.css'
+import EvenAndOdd from './components/EvenAndOdd/EvenAndOdd'
+import FilterObject from './components/FilterObject/FilterObject'
+import FilterString from './components/FilterString/FilterString'
+import Palindrome from './components/Palindrome/Palindrome'
+import Sum from './components/Sum/Sum'
 
 class App extends Component {
   render() {
-    
+	  return (
+		  <div>
+		  	<EvenAndOdd />
+		  	<FilterObject />
+		  	<FilterString />
+		  	<Palindrome />
+		  	<Sum />
+		  </div>
+	  )
   }
+
 }
 
 export default App;
